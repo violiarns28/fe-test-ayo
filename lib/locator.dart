e633dc9dca121cd5f02d782470a6d2fcd6f48d8f
Violia Ruana <violiaruana@gmail.com>
2025-09-01 23:13:08 +0700

    feat: add service locator setup for dependency injection

---
+++ lib/locator.dart
@@ -0,0 +1,38 @@
+import 'package:fe_test_ayo/features/community/data/data_sources/local_community_data_source.dart';
+import 'package:fe_test_ayo/features/community/data/repositories/community_repository.dart';
+import 'package:fe_test_ayo/features/community/domain/repositories/community_repository.dart';
+import 'package:fe_test_ayo/features/tournament/data/data_sources/local_tournament_data_source.dart';
+import 'package:fe_test_ayo/features/tournament/data/repositories/tournament_repository.dart';
+import 'package:fe_test_ayo/features/tournament/domain/repositories/tournament_repository.dart';
+import 'package:fe_test_ayo/features/user/data/data_sources/local_user_data_source.dart';
+import 'package:fe_test_ayo/features/user/data/repositories/user_repository.dart';
+import 'package:fe_test_ayo/features/user/domain/repositories/user_repository.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupLocator() {
+  setupDataSource();
+  setupRepository();
+  setupCubit();
+}
+
+void setupDataSource() {
+  getIt.registerSingleton(LocalUserDataSource());
+  getIt.registerSingleton(LocalCommunityDataSource());
+  getIt.registerSingleton(LocalTournamentDataSource());
+}
+
+void setupRepository() {
+  getIt.registerSingleton<IUserRepository>(
+    UserRepository(getIt<LocalUserDataSource>()),
+  );
+  getIt.registerSingleton<ICommunityRepository>(
+    CommunityRepository(getIt<LocalCommunityDataSource>()),
+  );
+  getIt.registerSingleton<ITournamentRepository>(
+    TournamentRepository(getIt<LocalTournamentDataSource>()),
+  );
+}
+
+void setupCubit() {}
