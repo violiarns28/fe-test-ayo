a4361e455358b24f4d64bf81a7ae3131c09c2f1e
Violia Ruana <violiaruana@gmail.com>
2025-09-01 23:54:27 +0700

    refactor: update TournamentRepository and ITournamentRepository to use generic types for TournamentModel

--- lib/features/tournament/data/repositories/tournament_repository.dart
+++ lib/features/tournament/data/repositories/tournament_repository.dart
@@ -1,20 +1,20 @@
 import 'package:fe_test_ayo/features/tournament/data/data_sources/local_tournament_data_source.dart';
-import 'package:fe_test_ayo/features/tournament/domain/entities/tournament_entity.dart';
+import 'package:fe_test_ayo/features/tournament/data/models/tournament_model.dart';
 import 'package:fe_test_ayo/features/tournament/domain/repositories/tournament_repository.dart';
 
-class TournamentRepository implements ITournamentRepository {
-  final LocalTournamentDataSource localDataSource;
+class TournamentRepository<T> implements ITournamentRepository<T> {
+  final LocalTournamentDataSource<T> localDataSource;
 
   TournamentRepository(this.localDataSource);
 
   @override
-  Future<List<TournamentEntity>> getAll() {
+  Future<List<TournamentModel<T>>> getAll() {
     final tournamentModels = localDataSource.getAllTournaments();
     return Future.value(tournamentModels);
   }
 
   @override
-  Future<TournamentEntity> getById(String id) {
+  Future<TournamentModel<T>> getById(String id) {
     final tournamentModel = localDataSource.getTournamentById(id);
     if (tournamentModel != null) {
       return Future.value(tournamentModel);
--- lib/features/tournament/domain/repositories/tournament_repository.dart
+++ lib/features/tournament/domain/repositories/tournament_repository.dart
@@ -1,6 +1,6 @@
 import 'package:fe_test_ayo/features/tournament/domain/entities/tournament_entity.dart';
 
-abstract class ITournamentRepository {
-  Future<List<TournamentEntity>> getAll();
-  Future<TournamentEntity> getById(String id);
+abstract class ITournamentRepository<T> {
+  Future<List<TournamentEntity<T>>> getAll();
+  Future<TournamentEntity<T>> getById(String id);
 }
