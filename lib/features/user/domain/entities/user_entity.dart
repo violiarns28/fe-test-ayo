81fd0fe510ea795b1a445ef1e85c98d9cc29363b
Violia Ruana <violiaruana@gmail.com>
2025-09-01 21:22:35 +0700

    feat: add UserEntity class with required fields

---
+++ lib/features/user/domain/entities/user_entity.dart
@@ -0,0 +1,9 @@
+abstract class UserEntity {
+  final String id;
+  final String name;
+  final String username;
+  final String image;
+
+  const UserEntity({required this.id, required this.name, required this.username, required this.image});
+}
+
