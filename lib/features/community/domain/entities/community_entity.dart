3f65c1069ea77edc584228a37fb25ee3d2f77a7f
Violia Ruana <violiaruana@gmail.com>
2025-09-01 21:31:34 +0700

    feat: add CommunityEntity class with required fields

---
+++ lib/features/community/domain/entities/community_entity.dart
@@ -0,0 +1,13 @@
+abstract class CommunityEntity {
+  final String id;
+  final String name;
+  final String region;
+  final String image;
+
+  const CommunityEntity({
+    required this.id,
+    required this.name,
+    required this.region,
+    required this.image,
+  });
+}
