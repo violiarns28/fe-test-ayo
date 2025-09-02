bb22feb99f9d7f496d7d816c8d6e1d8b2feb477a
Violia Ruana <violiaruana@gmail.com>
2025-09-02 11:49:39 +0700

    feat: add route for tournament details in router

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,16 @@
 import 'package:fe_test_ayo/features/tournament/presentation/screens/home_screen.dart';
+import 'package:fe_test_ayo/features/tournament/presentation/screens/tournament_detail_screen.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => HomeScreen())],
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
+    GoRoute(
+      path: '/tournament/:id',
+      builder: (context, state) {
+        final tournamentId = state.pathParameters['id']!;
+        return TournamentDetailScreen(tournamentId: tournamentId);
+      },
+    ),
+  ],
 );
